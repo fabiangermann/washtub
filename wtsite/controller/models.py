b9963e2cf6f2af4aa5d5c8a9662781bd1a464fff
Chris Everest <everestx@users.sourceforge.net>
2009-03-13 16:31:04 +0000

    A

--- wtsite/controller/models.py
+++ wtsite/controller/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Setting(Models.model):
+    value = CharField(max_length=128)
+    data = CharField()
+    hostname = CharField(max_length=255)
--- wtsite/urls.py
+++ wtsite/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
+from django.contrib.auth.views import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
+admin.autodiscover()
 urlpatterns = patterns('',
     # Example:
     # (r'^wtsite/', include('wtsite.foo.urls')),
@@ -13,5 +16,11 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
+    (r'^washtub/admin/(.*)', admin.site.root),
+        
+    #Provide password reset 
+    (r'^washtub/password_reset/$', 'django.contrib.auth.views.password_reset'),
+    (r'^washtub/password_reset/done/$', 'django.contrib.auth.views.password_reset_done'),
+    (r'^washtub/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm'),
+    (r'^washtub/reset/done/$', 'django.contrib.auth.views.password_reset_complete'),
 )
