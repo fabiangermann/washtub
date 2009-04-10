348c1b45e0954ec786adb16d580344a19298a646
Chris Everest <everestx@users.sourceforge.net>
2009-04-10 15:15:31 +0000

    Updated all url.py files to include settings.BASE_PATH

--- wtsite/mediapool/urls.py
+++ wtsite/mediapool/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('wtsite.mediapool.views',     
-    (r'^mediapool/scan$', 'file_scanner'),
+    (r'^scan$', 'file_scanner'),
     #(r'^$', 'index'),
     )
--- wtsite/urls.py
+++ wtsite/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.contrib.auth.views import *
+from django.conf import settings
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -10,22 +12,22 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Example:
     # (r'^wtsite/', include('wtsite.foo.urls')),
-    (r'^washtub/', include('wtsite.controller.urls')),
-    (r'^washtub/', include('wtsite.mediapool.urls')),
+    (r'^%s/', settings.BASE_URL, include('wtsite.controller.urls')),
+    (r'^%s/mediapool/', settings.BASE_URL, include('wtsite.mediapool.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^washtub/admin/(.*)', admin.site.root),
+    (r'^%s/admin/(.*)', admin.site.root),
     
     # Add Login URLS
-    (r'^washtub/login/$', 'django.contrib.auth.views.login'),
-    (r'^washtub/logout/$', 'django.contrib.auth.views.logout_then_login'),
+    (r'^%s/login/$', settings.BASE_URL, 'django.contrib.auth.views.login'),
+    (r'^%s/logout/$', settings.BASE_URL, 'django.contrib.auth.views.logout_then_login'),
     #Provide password reset 
-    (r'^washtub/password_reset/$', 'django.contrib.auth.views.password_reset'),
-    (r'^washtub/password_reset/done/$', 'django.contrib.auth.views.password_reset_done'),
-    (r'^washtub/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm'),
-    (r'^washtub/reset/done/$', 'django.contrib.auth.views.password_reset_complete'),
+    (r'^%s/password_reset/$', settings.BASE_URL, 'django.contrib.auth.views.password_reset'),
+    (r'^%s/password_reset/done/$', settings.BASE_URL, 'django.contrib.auth.views.password_reset_done'),
+    (r'^%s/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', settings.BASE_URL, 'django.contrib.auth.views.password_reset_confirm'),
+    (r'^%s/reset/done/$', settings.BASE_URL, 'django.contrib.auth.views.password_reset_complete'),
 )
