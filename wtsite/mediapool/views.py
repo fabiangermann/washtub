57975c728c6a4ce5b0b0f5634d749c719a047a68
Chris Everest <everestx@users.sourceforge.net>
2009-04-26 19:50:26 +0000

    Attempt at speeding up the scan process. Created build_file_list2 and clean_db2

--- wtsite/mediapool/views.py
+++ wtsite/mediapool/views.py
@@ -33,10 +33,37 @@ def build_file_list(dir):
                     s.save()
     return
 
+def build_file_list2(dir):
+    if not (access(dir, (F_OK or R_OK))):
+        return
+    #empty all songs from current database.  This should be fast!!!
+    d = Song.objects.all()
+    d.delete()
+    list = walk(dir,topdown=True)
+    for root, dirs, files in list:
+        for f in files:
+            ext = path.splitext(f)[1]
+            if ext in ('.mp3', '.flac'):
+                full_path = path.join(root,f)
+                #mod_time = stat(full_path)[ST_MTIME]
+                #mod_time = datetime.datetime.fromtimestamp(mod_time)
+                #try: 
+                #    #check update time and compare against database.
+                #    s = Song.objects.get(filename=full_path)
+                #    if(mod_time > s.date_modified): 
+                #        s.date_modified=mod_time.isoformat(' ')
+                #        s.save()
+                #except Song.DoesNotExist:
+                    #add it into the database
+                now = datetime.datetime.now().isoformat(' ')
+                s = Song(filename=full_path, date_modified=mod_time, date_entered=now)
+                s.save()
+    return
+
 def clean_db(dir, songs):
     if not (access(dir, (F_OK or R_OK))):
         return
-    # remove songs that aren't physically on the filesystem
+    # remove songs that are in the database, but aren't physically on the filesystem
     for s in songs:
         found = False
         db_filename = smart_str(s.filename)
@@ -56,7 +83,7 @@ def clean_db(dir, songs):
             d = Song.objects.get(filename__exact=s.filename)
             d.delete()
     
-    # refresh list of songs (we may have just deletes some)
+    # refresh list of songs (we may have just deleted some)
     songs = Song.objects.all()
     # remove albums that don't have corresponding songs
     d = Album.objects.filter(song__title__isnull=True)
@@ -70,6 +97,22 @@ def clean_db(dir, songs):
     
     return  
 
+def clean_db2(dir, songs):
+    if not (access(dir, (F_OK or R_OK))):
+        return
+    # No need to remove objects that don't exist (build_file_list2 only adds new to empty database)
+    # Remove albums that don't have corresponding songs
+    d = Album.objects.filter(song__title__isnull=True)
+    d.delete()
+    
+    d = Artist.objects.filter(song__title__isnull=True)
+    d.delete()
+    
+    d = Genre.objects.filter(song__title__isnull=True)
+    d.delete()
+    
+    return 
+
 @login_required()
 def file_scanner(request):
     
@@ -78,9 +121,9 @@ def file_scanner(request):
     else:
         return
     
-    list = build_file_list(directory)
+    list = build_file_list2(directory)
     songs = Song.objects.all()
-    clean_db(directory, songs)
+    clean_db2(directory, songs)
     
     return HttpResponseRedirect('/'+settings.BASE_URL)
     
